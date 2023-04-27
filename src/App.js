c8fe4d09753602b2bc1313b83288032ee7ecdee0
adrian <adrian.reyes@zahonero.com>
2023-04-27 17:18:33 +0200

    signin, signup, nav y pistas establecido

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Nav } from './components/Nav';
 import { Signin } from './components/Signin';
 import { Signup } from './components/signup';
+import { NoPage } from './components/NoPage';
+import { Home } from './components/Home';
+import { Pistas } from './components/Pistas';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Nav />}>
+          <Route index element={<Home />} />
           <Route path='signin' element={<Signin />} />
           <Route path='signup' element={<Signup />} />
+          <Route path='pistas' element={<Pistas />} />
+          <Route path='*' element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,104 @@
-import React from 'react';
+import * as React from 'react';
 import { Outlet, Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
 export const Nav = () => {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/signin">Sign in</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign up</Link>
-          </li>
-        </ul>
-      </nav>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Link to="/" className='enlaces'>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                sx={{
+                  mr: 2,
+                  display: { xs: 'none', md: 'flex' },
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                LOGO
+              </Typography>
+            </Link>
+
+            {/* Menú barra navegación. */}
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <Link to={'/pistas'} className='enlaces'>
+                <Button
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  Pistas
+                </Button>
+              </Link>
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <AccountCircleIcon />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {/* Menu usuario. */}
+                <Link to="/signin" className='enlaces'>
+                  <MenuItem onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">Sign in</Typography>
+                  </MenuItem>
+                </Link>
+                <Link to="/signup" className='enlaces'>
+                  <MenuItem onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">Sign up</Typography>
+                  </MenuItem>
+                </Link>
+
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
 
       <Outlet />
     </>
